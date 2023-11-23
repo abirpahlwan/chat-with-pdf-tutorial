01121edaa06115ba4dee6faa490c82b089d360df
abirpahlwan <abir978@gmail.com>
2023-11-23 09:04:42 +0600

    save and load index

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import os
+import pickle
+
+from dotenv import load_dotenv
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 # Config
+load_dotenv()
 st.set_page_config(page_title='Chat PDF', page_icon='🤖', layout='wide')
 
 
@@ -21,15 +30,42 @@ def sidebar():
 def main():
     # Header
     st.header('Chat with PDF')
-    st.subheader('Try it out!')
-
-    with st.container():
-        left_column, right_column = st.columns(2)
-        with left_column:
-            st.write('uhm...')
 
     # Upload a pdf file
     pdf = st.file_uploader('Upload your PDF', type='pdf')
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # st.write(text)
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+        # st.write(chunks)
+
+        # embeddings
+        db_name = pdf.name[:-4]  # remove last 4 chars .pdf
+        # st.write(f'{db_name}')
+
+        embeddings = OpenAIEmbeddings()
+
+        if os.path.exists(f'store/{db_name}'):
+            store = FAISS.load_local(f'store/{db_name}', embeddings)
+            st.write('Embeddings loaded from the disk')
+        else:
+            store = FAISS.from_texts(chunks, embeddings)
+            # FAISS.save_local(store, f'{db_name}.index')
+            store.save_local(f'store/{db_name}')
+            st.write('Embeddings saved to the disk')
 
     pass
 
